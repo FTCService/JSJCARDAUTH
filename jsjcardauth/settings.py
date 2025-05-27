4ce02a20a6dffcb5eed525799afe9ff30978a953
nitishjha74 <kumarnitishjha8877@gmail.com>
2025-05-27 16:38:40 +0530

    cross_origin_error

--- jsjcardauth/settings.py
+++ jsjcardauth/settings.py
@@ -167,3 +167,8 @@ STATIC_ROOT = os.path.join(BASE_DIR , 'staticfiles')
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+]
